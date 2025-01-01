ad7d7ca0597b11c31d81a4516e841cfcfeb29e66
marcosrabelocod <marcosrabelocod@gmail.com>
2025-01-01 18:19:11 -0300

    criação de container main

---
+++ src/components/Major.jsx
@@ -0,0 +1,13 @@
+import './Major.css'
+
+const Major = () => {
+  return (
+    <div className='start'>
+        <h1>Game Word</h1>
+        <p>clique para começar</p>
+        <button className='btn btn-light'>Start</button>
+    </div>
+  )
+}
+
+export default Major
